9501b4042732b092b6083cf1f0f4a561c72dcead
AmirHossin Mortezaee <129367158+amirhossin-mortezaee@users.noreply.github.com>
2025-10-23 15:17:00 +0330

    Clean .vs folder and update .gitignore

---
+++ CleanShop.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using CleanShop.Domain.Common;
+
+namespace CleanShop.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
